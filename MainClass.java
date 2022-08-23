b200d8898f5cd67edc85dffd202f4847fe0ad9e8
Artur <46168893+MeEu1@users.noreply.github.com>
2022-08-23 14:56:59 -0300

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,11 @@
+package p1;
+
+public class MainClass {
+	public static void main(String args[]) {
+		int n1 = 5;
+		int n2 = 10;
+		int total = n1 + n2;
+		
+		System.out.println("A soma de " + n1 + " + " + n2 + " é: " + total);
+	}
+}
